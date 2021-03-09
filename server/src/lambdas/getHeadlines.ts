adb5ec123dcb050571682c6e45ac3727d1022f60
Liam <rockinliam@gmail.com>
2021-03-09 01:58:32 +0000

    s3 get headlines

--- server/src/lambdas/getHeadlines.ts
+++ server/src/lambdas/getHeadlines.ts
@@ -1,45 +1,32 @@
 import {APIGatewayEvent, APIGatewayProxyResult, Handler} from 'aws-lambda';
+import AWS, {S3} from 'aws-sdk'
 import {responses} from "../../common/responses";
-import { Team, Person, PersonsResponse, TeamResponse } from '../../common/interfaces';
-import {getAllTeams, getPerson, getTeam} from "../../common/data";
+import { Team, Person, MinPerson, TeamsResponse } from '../../common/interfaces';
+import {getAllPeople, getAllTeams, getPerson} from "../../common/data";
+import {S3Parameters} from "aws-sdk/clients/quicksight";
 
 export const handler: Handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult>  => {
     console.log("Incoming event:");
     console.log(JSON.stringify(event));
+    const s3: S3 = new AWS.S3();
 
-    if(!event.pathParameters || !event.pathParameters.ID) return responses.badRequest;
+    const options = {
+        Bucket: 'headlines-bucket',
+        Key: 'headlines.json',
+    };
 
-    try {
-        let team: Team = await getTeam(event.pathParameters.ID);
-        console.log(team);
+    const data = await s3.getObject(options).promise();
+    const fileContents = data.Body.toString();
+    const json = JSON.parse(fileContents);
+    console.log(json);
 
-        const people: string[] = team.people as string[];
-        console.log(people);
-
-        const fullPeople: Person[] = [];
-        for await (const id of people) {
-            //get person
-            const fullPerson = await getPerson(id);
-            console.log(fullPerson);
-
-            fullPeople.push(fullPerson);
-        }
-
-        fullPeople.forEach((item) => {
-            item.slackID = item.slackID.replace(" ", "-");
-        });
-
-        team.people = fullPeople;
-        console.log(team);
-
+    if(data){
         const res: APIGatewayProxyResult = responses.ok;
-        res.body = JSON.stringify({ data: team } as TeamResponse);
-        return res;
-
-    } catch (error) {
-        console.log(error)
-        const res: APIGatewayProxyResult = responses.serverError;
-        res.body = error;
+        res.body = json;
         return res;
     }
+
+    const res: APIGatewayProxyResult = responses.ok;
+    res.body = "Nothing found."
+    return res;
 };
